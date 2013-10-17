ed943918fe26edaab7b74a893dbc69e6909b0aa6
syang <syang@local>
2013-10-17 17:16:57 +0800

    show more fields

--- TTPieView.m
+++ TTPieView.m
@@ -18,6 +18,15 @@
     return self;
 }
 
+static float gColors[][3] = {
+    {0.2588, 0.3490, 0.8510},
+    {0.0000, 0.6314, 0.8431},
+    {0.0000, 0.8706, 0.4667},
+    {0.9333, 0.9804, 0.0000},
+    {0.9922, 0.7373, 0.1255},
+    {1.0000, 0.1412, 0.1961},
+};
+
 - (void)setObject:(id)item {
     double sum = 0;
     for (NSNumber *number in item) {
@@ -35,8 +44,8 @@
         layer.startAngle = startAngle;
         layer.endAngle = endAngle;
 
-        UIColor *color = [UIColor colorWithRed:i % 3 == 0 green:i % 3 == 1 blue:i % 3 == 2 alpha:.5];
-        layer.fillColor = color.CGColor;
+        float *color = gColors[i % 6];
+        layer.fillColor = [UIColor colorWithRed:color[0] green:color[1] blue:color[2] alpha:.5].CGColor;
 
         [self.layer addSublayer:layer];
         startAngle = endAngle;
